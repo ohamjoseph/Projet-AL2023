4c6a56aa2ed80316b3b31be1fd338b7d21cc58fe
shado <ouilyh@gmail.com>
2023-04-05 14:11:29 +0000

    ajout de colis et de positions

--- delivery_client/api/views.py
+++ delivery_client/api/views.py
@@ -23,14 +23,66 @@ class ListColis(APIView):
     
     def post(self, request):
         data = request.data
+        coli = {
+            'client':data['client'],
+            'numeros_colis':data['numero'],
+        }
         
-        response = requests.post(tools.coli_url, json=data)
-        if not response.ok:
-            response = response.json()
+        response_coli = requests.post(tools.coli_url, json=coli)
+        if not response_coli.ok:
+            response_coli = response_coli.json()
+        else:
+            location = data['latitude']+','+data['longitude']
+            position = {
+                'location': location,
+                'coli': data['numero']
+            }
+            response_position = requests.post(tools.posotion_url, json=position)
+            
+            if not response_position.ok:
+                response_position = response_position.json()
         
         list_colis = {
-            'colis_data': response,
+            'colis_data': response_coli,
+            'position_data': response_position,
         }
         
         return Response(list_colis)
+    
+    
+class ListPosition(APIView):
+    def get(self, request):
+        numero = request.GET.get('numero',None)
+        if numero is not None:
+            position_data = requests.get(tools.posotion_url,params={"coli": numero})
+        else:
+            position_data = requests.get(tools.posotion_url)
+            
+        # Traitement des données et formatage
+        list_colis = {
+            'colis_data': position_data.json(),
+        }
         
+        # Renvoi des données agrégées sous forme de réponse HTTP
+        return Response(list_colis)
+    
+    def post(self, request):
+        data = request.data
+    
+        
+        location = data['latitude']+','+data['longitude']
+        position = {
+            'location': location,
+            'coli': data['numero']
+        }
+        response_position = requests.post(tools.posotion_url, json=position)
+        
+        if not response_position.ok:
+            response_position = response_position.json()
+        
+        list_colis = {
+            'position_data': response_position.json(),
+        }
+        
+        return Response(list_colis)
+    
